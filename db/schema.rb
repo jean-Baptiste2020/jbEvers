6e2780647eecd9954138133f9f24da67507ee773
Chabla <chabijeanbaptiste@gmail.com>
2021-10-20 16:19:22 +0000

    step2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_113345) do
+ActiveRecord::Schema.define(version: 2021_10_20_113346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_20_113345) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "expiry_date"
   end
 
 end
